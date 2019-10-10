1423b8816e96595f6648149d318a21dca0298ff7
dineshGH0062 <36599870+dineshGH0062@users.noreply.github.com>
2019-10-10 12:42:44 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-C:\Users\FRESCANO143\AppData\Local\Programs\Python\Python37\python.exe --alluredir=reports\allure_reports\ -m pytest 
+C:\Users\FRESCANO143\AppData\Local\Programs\Python\Python37\python.exe  -m pytest --alluredir=reports\allure_reports\
